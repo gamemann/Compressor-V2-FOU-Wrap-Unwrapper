6fc79925698978234dd8a17a62d97694c49cbbae
Christian Deacon <gamemann@gflclan.com>
2020-05-18 18:46:34 +0000

    Updated Test BPF.

--- src/testBPF.c
+++ src/testBPF.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
 #include <inttypes.h>
 
 #include "../libbpf/src/bpf.h"
 #include "../libbpf/src/libbpf.h"
+#include "include/common.h"
+
+#include <errno.h>
+#include <error.h>
+#include <string.h>
+
+extern int errno;
 
 #define BASEDIR_MAPS "/sys/fs/bpf/tc/globals"
 
 const char *info_map = BASEDIR_MAPS "/info_map";
-const char *ip_map = BASEDIR_MAPS "/ip_map";
 
-int info_map_fd, ip_map_fd;
-
-struct packet_info
-{
-    uint32_t popIP;
-    uint32_t gameIP;
-    //uint32_t interalIP;
-    uint16_t port;
-};
+int info_map_fd;
 
 int open_map(const char *name)
 {
@@ -44,18 +43,36 @@ int open_map(const char *name)
 int main()
 {
     info_map_fd = open_map(info_map);
-    ip_map_fd = open_map(ip_map);
 
-    uint32_t key = 0;
+    if (info_map_fd < 1)
+    {
+        printf("Error getting info map.\n");
+
+        return 0;
+    }
+
+    uint32_t key = inet_addr("10.50.0.21");
+
+    printf("Updating key %" PRIu32 "\n", key);
 
     struct packet_info *pcktInfo;
-    pcktInfo->popIP = inet_addr("10.50.0.3");
-    //pcktInfo->interalIP = inet_addr("192.168.5.2");
+    pcktInfo->popIP = inet_addr("10.50.0.21");
+    pcktInfo->internalIP = inet_addr("172.20.0.2");
     pcktInfo->gameIP = inet_addr("10.50.0.4");
     pcktInfo->port = htons(1337);
+    
+    printf("Using internal IP => %" PRIu32 "\n", pcktInfo->internalIP);
 
-    bpf_map_update_elem(info_map_fd, &key, &pcktInfo, BPF_ANY);
-    bpf_map_update_elem(ip_map_fd, &key, &pcktInfo->popIP, BPF_ANY);
+    if (bpf_map_update_elem(info_map_fd, &key, pcktInfo, BPF_ANY) != 0)
+    {
+        printf("Error updating map. Error num => %d. Error string => %s\n", errno, strerror(errno));
+    }
+
+    struct packet_info *newinfo = malloc(sizeof(struct packet_info));
+
+    bpf_map_lookup_elem(info_map_fd, &key, newinfo);
+
+    printf("Internal address from lookup => %" PRIu32 "\n", newinfo->internalIP);
 
     fprintf(stdout, "Updated BPF maps...\n");
 
